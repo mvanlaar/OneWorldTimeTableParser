a878429abf73ef1f032b22340bd859d963e108d1
mvanlaar <martijn@mvanlaar.net>
2015-06-11 14:01:13 +0200

    namespace change

--- AirlineTimeTableParser/CSVSplitter.cs
+++ AirlineTimeTableParser/CSVSplitter.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace AirlineTimeTableParser
+namespace OneworldTimeTableParser
 {
     // mangled code horribly to fit without scrolling
     public static class CsvSplitter
